c2909ebe8988f0fc1782a00557efa38bfd9c073c
gomarcd <git@gomarcd.dev>
2024-10-19 10:44:56 -0600

    fill archive + bookmark model

--- app/Models/Archive.php
+++ app/Models/Archive.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Archive extends Model
 {
-    //
+    protected $fillable = [
+        'bookmark_id',
+        'archive_service',
+        'archive_url',
+        'archived_at'
+    ];
+
+    public function bookmark()
+    {
+        return $this->belongsTo(Bookmark::class);
+    }
 }
--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bookmark extends Model
 {
-    //
+    protected $fillable = [
+        'url',
+        'title',
+        'description',
+        'user_id'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function archives()
+    {
+        return $this->hasMany(Archive::class);
+    }
 }

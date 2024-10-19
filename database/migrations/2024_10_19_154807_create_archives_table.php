3acce488167d7d9f2615cd4e0f3421541be0b753
gomarcd <git@gomarcd.dev>
2024-10-19 10:41:48 -0600

    add migration+model for bookmark + archive

---
+++ database/migrations/2024_10_19_154807_create_archives_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('archives', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bookmark_id')
+                  ->constrained('bookmarks')
+                  ->onDelete('cascade');
+            $table->string('archive_service');
+            $table->string('archive_url');
+            $table->timestamp('archived_at');
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('archives');
+    }
+};
